6404855381191675debe4eeecc123f11bcd5f8f1
CodekaUp <CodekaUp@users.noreply.github.com>
2023-05-23 17:03:11 +0300

    Добавьте файлы проекта.

---
+++ Sort/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Sort_Library;
+using System.Reflection;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Получение текстового файла. Нужно ввести полный путь к файлу
+        string filePath = Console.ReadLine();
+
+        //Получение приватного метода из dll
+        var library = typeof(Library);
+        MethodInfo method = typeof(Library).GetMethod("GetWordCounts", BindingFlags.NonPublic | BindingFlags.Static);
+        Dictionary<string, int> wordCounts = (Dictionary<string, int>)method.Invoke(library, new object[] {filePath});
+
+        //Проверяем содержит ли словарь какие-либо элементы, если да - создаем файл "output.txt", перечисляя слова в порядке убывания их кол-ва
+        if (wordCounts.Any())
+        {
+            string outputFilePath = Path.Combine(Path.GetDirectoryName(filePath), "output.txt");
+
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                foreach (KeyValuePair<string, int> v in wordCounts.OrderByDescending(x => x.Value))
+                {
+                    writer.WriteLine($"{v.Key} - {v.Value}");
+                }
+            }
+
+            Console.WriteLine($"Результат сохранен в файл {outputFilePath}");
+        }
+        else
+        {
+            Console.WriteLine("Файл не содержит слов");
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ Sort_Library/Library.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Sort_Library
+{
+    public static class Library
+    {
+        private static Dictionary<string, int> GetWordCounts(string filePath)
+        {
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line = " ";
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        foreach (string word in Regex.Split(line.ToLower(), @"\W+"))
+                        {
+                            if (!string.IsNullOrEmpty(word))
+                            {
+                                if (wordCounts.ContainsKey(word))
+                                {
+                                    wordCounts[word]++;
+                                }
+                                else
+                                {
+                                    wordCounts.Add(word, 1);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return wordCounts;
+        }
+    }
+}
