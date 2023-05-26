986be79bac880ee25ac3b01d5fde8f07fb8861db
CodekaUp <CodekaUp@users.noreply.github.com>
2023-05-26 19:42:49 +0300

    Выполнено задание по блоку Процессы и Потоки:
    1. Добавлен публиный метод с многопоточностью
    2. Добавлен вызов публичного метода
    3. Добавлен вывод времени выполнения методов

--- Sort_Library/Library.cs
+++ Sort_Library/Library.cs
@@ -1,9 +1,13 @@
 ﻿using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Sort_Library
 {
-    public static class Library
+    public class Library
     {
+        
+
         private static Dictionary<string, int> GetWordCounts(string filePath)
         {
             Dictionary<string, int> wordCounts = new Dictionary<string, int>();
@@ -30,6 +34,46 @@ namespace Sort_Library
                                 }
                             }
                         }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return wordCounts;
+        }
+
+
+        public static Dictionary<string, int> GetWordCountsParallel(string filePath)
+        {
+
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line = " ";
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        //многопоточность
+                        Parallel.ForEach(Regex.Split(line.ToLower(), @"\W+"), word =>
+                        {
+                            if (!string.IsNullOrEmpty(word))
+                            {
+                                if (wordCounts.ContainsKey(word))
+                                {
+                                    wordCounts[word]++;
+                                }
+                                else
+                                {
+                                    wordCounts.Add(word, 1);
+                                }
+                            }
+                        });
                     }
                 }
             }
